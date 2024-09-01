b381b7e2d4513c4270275814d212cc2ccaa6216a
Jacob Sanders <faolan@rhubarbvr.net>
2024-09-01 11:26:54 -0400

    First

---
+++ UniversalHybridTemplate/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace UniversalHybridTemplate
+{
+	public partial class App : Application
+	{
+		public App() {
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
---
+++ UniversalHybridTemplate/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversalHybridTemplate
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage() {
+			InitializeComponent();
+		}
+	}
+}
